913796dfa84dd4b7149a9cfa2bca85f198da95a7
Marko Markovic <markomarkoviccb@gmail.com>
2024-11-07 17:23:49 +0100

    fix eslint errors

--- party-up-frontend/eslint.config.mjs
+++ party-up-frontend/eslint.config.mjs
@@ -1,12 +1,12 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
-import pluginPrettier from 'eslint-plugin-prettier';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
+    files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: {
       globals: globals.browser,
     },
@@ -14,16 +14,23 @@ export default [
       prettier: pluginPrettier,
     },
     rules: {
-      'no-console': 'warn',
-      'no-unused-vars': 'warn',
-      quotes: ['error', 'single'],
-      semi: ['error', 'always'],
-      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-      'react/prop-types': 'off',
-      'prettier/prettier': 'error',
+      "no-console": "warn",
+      "no-unused-vars": "warn",
+      quotes: ["error", "single"],
+      semi: ["error", "always"],
+      "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+      "react/prop-types": "off",
+      "prettier/prettier": "error",
     },
   },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   eslintConfigPrettier,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ];
