f6ded99db2024b515e02093c1b3d8c1325056ffa
Marko Markovic <markomarkoviccb@gmail.com>
2024-11-07 14:53:44 +0100

    first commit

---
+++ party-up-service/src/main/java/com/party_up/network/PartyUpApplication.java
@@ -0,0 +1,13 @@
+package com.party_up.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyUpApplication.class, args);
+	}
+
+}
---
+++ party-up-service/src/test/java/com/party_up/network/PartyUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.party_up.network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
