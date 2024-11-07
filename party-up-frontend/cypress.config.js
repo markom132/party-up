bc3211548952a166c1d22e5905cc8f1fb1a66335
Marko Markovic <markomarkoviccb@gmail.com>
2024-11-07 19:13:38 +0100

    fix eslint errors

--- party-up-frontend/cypress.config.js
+++ party-up-frontend/cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/integration/**/*.{js,jsx,ts,tsx}", // Update this to match your test files' path
-    setupNodeEvents(on, config) {
+    specPattern: 'cypress/integration/**/*.{js,jsx,ts,tsx}', // Update this to match your test files' path
+    setupNodeEvents() {
       // implement node event listeners here
     },
   },
--- party-up-frontend/src/App.js
+++ party-up-frontend/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
-          Welcome
-      </header>
+      <header className="app-header">Welcome</header>
     </div>
   );
 }
