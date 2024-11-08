472fcfbbc48bd0da0f0611c5e97a66e228ec5014
Marko Markovic <markomarkoviccb@gmail.com>
2024-11-08 17:09:18 +0100

    add id to user entity

--- party-up-service/src/main/java/com/party_up/network/model/User.java
+++ party-up-service/src/main/java/com/party_up/network/model/User.java
@@ -2,6 +2,9 @@ package com.party_up.network.model;
 
 import jakarta.persistence.Entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +14,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String username;
 
     private String password;
