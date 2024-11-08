404b982f9edfed850eb8d9a2815cffab0ad20d72
Marko Markovic <markomarkoviccb@gmail.com>
2024-11-08 17:41:53 +0100

    Implement_User_Entity_Repository_Controller_Service closes #6

--- party-up-service/src/main/java/com/party_up/network/model/User.java
+++ party-up-service/src/main/java/com/party_up/network/model/User.java
@@ -1,13 +1,15 @@
 package com.party_up.network.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.party_up.network.model.enums.AccountStatus;
+import jakarta.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -19,7 +21,49 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
 
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private AccountStatus status;
+
+    @Lob
+    private byte[] image;
+
+    private LocalDateTime birthDate;
+
+    private int age;
+
+    private String bio;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime lastUpdatedAt;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AuthToken> authTokens;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        lastUpdatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        lastUpdatedAt = LocalDateTime.now();
+    }
 }
---
+++ party-up-service/src/main/java/com/party_up/network/model/enums/AccountStatus.java
@@ -0,0 +1,18 @@
+package com.party_up.network.model.enums;
+
+/**
+ * Enum representing the status of a User/Account.
+ * The user can either be ACTIVE or INACTIVE.
+ */
+public enum AccountStatus {
+
+    /**
+     * Indicates that the user is currently active.
+     */
+    ACTIVE,
+
+    /**
+     * Indicates that user is currently inactive.
+     */
+    INACTIVE
+}
