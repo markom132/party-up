43397067193eb08af4af135281f255bb1329c80e
Marko Markovic <markomarkoviccb@gmail.com>
2024-11-08 17:10:54 +0100

    fix checkstyle issues

--- party-up-service/src/main/java/com/party_up/network/model/User.java
+++ party-up-service/src/main/java/com/party_up/network/model/User.java
@@ -1,10 +1,10 @@
 package com.party_up.network.model;
 
 import jakarta.persistence.Entity;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
