f19afc490202fb52627c677c3efeb19c3643f84e
Marko Markovic <markomarkoviccb@gmail.com>
2024-11-26 15:18:30 +0100

    fixing prettier issues

--- party-up-frontend/src/components/__tests__/HeroSection.test.tsx
+++ party-up-frontend/src/components/__tests__/HeroSection.test.tsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import HeroSection from "../HeroSection/HeroSection";
+import HeroSection from '../HeroSection/HeroSection';
 
 describe('HeroSection Component', () => {
-    test('Renders HeroSection', () => {
-        const userName: string = 'Marko';
-        render(<HeroSection userName={userName} />);
+  test('Renders HeroSection', () => {
+    const userName: string = 'Marko';
+    render(<HeroSection userName={userName} />);
 
-        expect(screen.getByText('Welcome back, Marko! Ready to party?'));
-        expect(screen.getByText('Connect. Organize. Attend.'));
-        expect(screen.getByText('Party-Up helps you find nearby events, meet people and share unforgettable moments.'));
-        expect(screen.getByText('Explore Events'));
-        expect(screen.getByText('Create Event'));
-        expect(screen.getByText('View Profile'));
-    });
+    expect(screen.getByText('Welcome back, Marko! Ready to party?'));
+    expect(screen.getByText('Connect. Organize. Attend.'));
+    expect(
+      screen.getByText(
+        'Party-Up helps you find nearby events, meet people and share unforgettable moments.',
+      ),
+    );
+    expect(screen.getByText('Explore Events'));
+    expect(screen.getByText('Create Event'));
+    expect(screen.getByText('View Profile'));
+  });
 });
